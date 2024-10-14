1c64d36c488b87e7bc544d7860c3337b26993057
vivek <vivekananda462@gmail.com>
2024-10-14 22:12:18 +0530

    Swiggy - FB Login

--- src/main/java/com/swiggyapp1/swiggyApp1/security/SecurityConfig.java
+++ src/main/java/com/swiggyapp1/swiggyApp1/security/SecurityConfig.java
@@ -17,38 +17,53 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests(
-            authorizeRequests ->
-                authorizeRequests
-                    .requestMatchers("/", "/oauth2/**", "/users/**")
-                    .permitAll()
-                    .anyRequest()
-                    .authenticated())
-        .oauth2Login(
-            oauth2Login ->
-                oauth2Login
-                    .defaultSuccessUrl("/oauth2/success", true)
-                    .failureUrl("/login?error=true"));
+                    authorizeRequests ->
+                            authorizeRequests
+                                    .requestMatchers("/", "/oauth2/**", "/users/**")
+                                    .permitAll()
+                                    .anyRequest()
+                                    .authenticated())
+            .oauth2Login(
+                    oauth2Login ->
+                            oauth2Login
+                                    .defaultSuccessUrl("/oauth2/success", true)
+                                    .failureUrl("/login?error=true"));
     return http.build();
   }
 
   @Bean
   public ClientRegistrationRepository clientRegistrationRepository() {
-    return new InMemoryClientRegistrationRepository(Arrays.asList(googleClientRegistration()));
+    return new InMemoryClientRegistrationRepository(
+            Arrays.asList(googleClientRegistration(), facebookClientRegistration()));
   }
 
   private ClientRegistration googleClientRegistration() {
     return ClientRegistration.withRegistrationId("google")
-        .clientId("427014481836-lh7kj12at8c45jmfar5im8c328tufait.apps.googleusercontent.com")
-        .clientSecret("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")
-        .scope("openid", "profile", "email")
-        .authorizationUri("https://accounts.google.com/o/oauth2/auth")
-        .tokenUri("https://oauth2.googleapis.com/token")
-        .userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
-        .userNameAttributeName("sub")
-        .clientName("Google")
-        .redirectUri("http://localhost:8080/login/oauth2/code/google")
-        .jwkSetUri("https://www.googleapis.com/oauth2/v3/certs")
-        .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-        .build();
+            .clientId("427014481836-lh7kj12at8c45jmfar5im8c328tufait.apps.googleusercontent.com")
+            .clientSecret("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")
+            .scope("openid", "profile", "email")
+            .authorizationUri("https://accounts.google.com/o/oauth2/auth")
+            .tokenUri("https://oauth2.googleapis.com/token")
+            .userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
+            .userNameAttributeName("sub")
+            .clientName("Google")
+            .redirectUri("http://localhost:8080/login/oauth2/code/google")
+            .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+            .build();
+  }
+
+  private ClientRegistration facebookClientRegistration() {
+    return ClientRegistration.withRegistrationId("facebook")
+            .clientId("3950478065187206")
+            .clientSecret("c3077c0ce0be4b88964efdca4c8af8be")
+            .scope("public_profile", "email")
+            .authorizationUri("https://www.facebook.com/v11.0/dialog/oauth")
+            .tokenUri("https://graph.facebook.com/v11.0/oauth/access_token")
+            .userInfoUri("https://graph.facebook.com/me?fields=id,name,email")
+            .userNameAttributeName("id")
+            .clientName("Facebook")
+            .redirectUri("http://localhost:8080/login/oauth2/code/facebook")
+            .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+            .build();
   }
 }
